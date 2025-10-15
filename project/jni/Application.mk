69043becf758d8c311cc3981b4a60ea1141b0ae3
rufusin <10528442+rufusin@users.noreply.github.com>
2025-10-15 16:39:25 +0300

    Update app platform

--- project/jni/Application.mk
+++ project/jni/Application.mk
@@ -1,5 +1,6 @@
 APP_STL := c++_static
 APP_CPPFLAGS += -fvisibility=hidden
-APP_PLATFORM := android-19
+APP_PLATFORM := android-21
+# APP_PLATFORM := android-19
 APP_ABI := armeabi-v7a arm64-v8a
-#APP_ABI := armeabi-v7a arm64-v8a x86_64 x86
+# APP_ABI := armeabi-v7a arm64-v8a x86_64 x86
